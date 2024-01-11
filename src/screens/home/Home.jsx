7e9f7cbd12ac0d2afcbe1f093bb6b09d4d0d36e6
abdulhaseebss <m.saleemhaseeb@gmail.com>
2024-01-11 23:28:41 +0500

    Todo App React

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,21 +1,25 @@
 import { useState } from 'react'
 import React from 'react'
 import { onAuthStateChanged , signOut } from "firebase/auth";
-import {auth} from "../../config/firebaseConfig/FirebaseConfig"
+import {auth , db} from "../../config/firebaseConfig/FirebaseConfig"
 import { useNavigate } from 'react-router-dom';
+import { collection, addDoc, Timestamp, getDocs , query ,orderBy, where, doc, deleteDoc } from "firebase/firestore"; 
 
 const Home = () => {
     const [text, setText] = useState("");
     const [todo, setTodo] = useState([]);
+    let newTodo = []
     const [promptValue, setPromptValue] = useState('');
     const navigate = useNavigate()
+    let uid;
 
     onAuthStateChanged(auth, (user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/auth.user
-        const uid = user.uid;
-        console.log(uid);
+        uid = user.uid;
+        // console.log(uid);
+        getDataFromFirestore()
         // ...
       } else {
         // User is signed out
@@ -24,8 +28,18 @@ const Home = () => {
       }
     });
       
-    
-    function addTodo(e) {
+    async function getDataFromFirestore() {
+      const q = await query(collection(db, "todos"), orderBy('postDate', 'desc') , where('uid' , '==' , uid));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // console.log(doc.data().todo);
+        newTodo.push(doc.data().todo)
+        // console.log(newTodo);
+      });
+      setTodo([...newTodo])
+    }
+
+    async function addTodo(e) {
       e.preventDefault()
       if (text == "") {
         alert('Enter Text')
@@ -36,14 +50,39 @@ const Home = () => {
       setTodo([...todo]);
       console.log(todo);
       setText("")
+      try {
+        const postObj = {
+          todo: text ,
+          uid: uid ,
+          postDate: Timestamp.fromDate(new Date()),
+        };
+        const docRef = await addDoc(collection(db, "todos"), postObj);
+        console.log("Document written with ID: ", docRef.id);
+        postObj.docId = docRef.id
+        newTodo = [postObj, ...todo]
+        console.log(newTodo);
+
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
     }
     
-    function deleteTodo(index) {
-  
-      todo.splice(index, 1);
-      setTodo([...todo]);
-      console.log(todo);
+    async function deleteTodo(index) {
+      console.log('delete called', newTodo[index]);
+
+      // await deleteDoc(doc(db, "todos", todo[index].docId))
+      //           .then(() => {
+      //               console.log('post deleted');
+      //               // todo.splice(index, 1);
+      //       }).catch((e)=>{
+      //         console.error("Error adding document: ", e);
+      //       })
+      // todo.splice(index, 1);
+      // setTodo([...todo]);
+      // console.log(todo);
+
     }
+
     function editTodo(index) {
       const userValue = prompt('Enter something:' , todo[index]);
       
@@ -83,9 +122,9 @@ const Home = () => {
         <button className='text-xl transition-all font-semibold hover:border-2 hover:border-blue-700 hover:rounded-xl px-4 py-2' type='submit'>Add</button>
       </form>
       </div>
-      <ul>
+      <ul key={'hello'}>
           {todo.map((item, index) => {
-            return <div ><li className='p-5 font-semibold'  key={index}>{item}
+            return <div key={index}><li className='p-5 font-semibold'  >{item}
             &nbsp;&nbsp;<button className=' bg-red-200 p-2 rounded-md mr-1 transition-all hover:bg-red-600 hover:text-white' onClick={()=>deleteTodo(index)}>Delete</button>&nbsp;
             <button className=' bg-slate-200 p-2 rounded-md transition-all hover:bg-slate-600 hover:text-white' onClick={()=>editTodo(index)}>Edit</button></li><hr /></div>
           })}
